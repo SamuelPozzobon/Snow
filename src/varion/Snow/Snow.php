285a4dad5adb4be184bc568a07c0d640d1978a9d
GoGoAirPlane <42131095+neoskr@users.noreply.github.com>
2020-12-06 12:17:14 +0900

    Update Snow.php

--- src/varion/Snow/Snow.php
+++ src/varion/Snow/Snow.php
@@ -4,38 +4,45 @@ namespace varion\Snow;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\Player;
-use pocketmine\entity\Entity;
-use pocketmine\block\Block;
-use pocketmine\item\Item;
-use pocketmine\network\mcpe\protocol\AddEntityPacket;
-use pocketmine\level\Position;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
+use pocketmine\level\biome\Biome;
 use pocketmine\event\level\ChunkLoadEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
 
-class Snow extends PluginBase implements Listener {
-    	public $cooltime = 0;
-	public $m_version = 2, $pk;
-	
-	public function onEnable() {
-        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+class Snow extends PluginBase implements Listener
+{
+
+	public function onEnable() : void
+	{
+
+		$this->levels = $this->getConfig()->getAll();
+		
+		$this->pk = new LevelEventPacket ();
+		$this->pk->evid = LevelEventPacket::EVENT_START_RAIN;
+		$this->pk->data = 10000;
+
+		$this->getServer()->getPluginManager()->registerEvents ($this, $this);
+
 	}
 	
-	public function onChunkLoadEvent(ChunkLoadEvent $event) {
+	public function onChunkLoadEvent(ChunkLoadEvent $event) : void
+	{
+		
+		if (! in_array ($event->getLevel()->getFolderName(), $this->levels))
+			return;
+
 		for($x = 0; $x < 16; ++ $x)
 			for($z = 0; $z < 16; ++ $z)
-				$event->getChunk()->setBiomeId ( $x, $z, 12);
+				$event->getChunk()->setBiomeId ($x, $z, Biome::ICE_PLAINS);
+
 	}
-	public function onPLayerJoin(PlayerJoinEvent $event) {
-		$player = $event->getPlayer ();
-		$pk = new LevelEventPacket ();
-		$pk->evid = 3001;
-		$pk->data = 10000;
-		$player->dataPacket ( $pk );
+
+	public function onPlayerJoin(PlayerJoinEvent $event) : void
+	{
+
+		if (in_array (($player = $event->getPlayer())->getLevel()->getFolderName(), $this->levels))
+			$player->dataPacket (clone $this->pk);
+
 	}
+
 }
-
-
